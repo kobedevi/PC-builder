586667e385f17cba5bac6b395eff68a63452aec3
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-11-16 19:19:03 +0100

    added components

--- app/src/components/App/Cpus/CpuOverview.js
+++ app/src/components/App/Cpus/CpuOverview.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useFetch from '../../../core/hooks/useFetch';
+import { fetchCpus } from '../../../core/modules/CPU/api';
 import { PossibleRoutes } from '../../../core/routing'
+import Alert from '../../Design/Alert';
+import Spinner from '../../Design/Spinner';
 
 const CpuOverview = () => {
+
+  const [info, setInfo] = useState();
+
+  const apiCall = useCallback(() => {
+    return fetchCpus();
+  }, [])
+
+  const {
+    data: cpus,
+    error,
+    setError,
+    isLoading,
+    refresh,
+  } = useFetch(apiCall);
+
   return (
     <>
-        <div>CpuOverview</div>
-        <Link className="nav-link" to={PossibleRoutes.CpuCreate}>Add Cpu</Link>
+      <h2>CpuOverview</h2>
+      {
+        error && <Alert color="danger">{error.message}</Alert>
+      }
+
+      <Link className="nav-link" to={PossibleRoutes.CpuCreate}>Add Cpu</Link>
+
+      {
+        isLoading && <Spinner/>
+      }
+      {
+        info && <Alert color="info">{info}</Alert>
+      }
+      
+      {
+        cpus && (
+          <ul>
+            {
+              cpus.map((cpu) => (
+                <li key={cpu.id_cpu}>
+                  {cpu.id_cpu}
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
     </>
   )
 }
