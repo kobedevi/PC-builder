f958d6502fbc86b926df4de3d102521e0da48893
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-12-09 13:56:42 +0100

    fixed validator length match

--- server/models/Manufacturer.js
+++ server/models/Manufacturer.js
@@ -1,10 +1,14 @@
-const {check} = require('express-validator');
+const { check } = require("express-validator");
 
 const manufacturerModel = [
-    check('manufacturerName').notEmpty().withMessage('manufacturerName can not be empty'),
-    check('manufacturerName').isLength({max:50}).withMessage('manufacturerName can not be longer than 50 characters'),
+	check("manufacturerName")
+		.notEmpty()
+		.withMessage("manufacturerName can not be empty"),
+	check("manufacturerName")
+		.isLength({ max: 70 })
+		.withMessage("manufacturerName can not be longer than 70 characters"),
 ];
 
 module.exports = {
-    manufacturerModel,
-}
+	manufacturerModel,
+};
