ffbfa072e620dc7804985945d1d96cf4366461a1
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-12-23 22:25:51 +0100

    corrected clockspeed error message

--- server/models/Cpu.js
+++ server/models/Cpu.js
@@ -1,13 +1,20 @@
-const {check} = require('express-validator');
+const { check } = require("express-validator");
 
 const cpuModel = [
-    check('modelName').notEmpty().isLength({max:100}).withMessage('modelName can not be empty'),
-    check('clockSpeed').isFloat({min:.1, max:100}).withMessage('cores must be of type float, with a max size of 4'),
-    check('clockSpeed').notEmpty().withMessage('clockSpeed can not be empty'),
-    check('cores').isInt({min:1, max: 100}).withMessage('cores must be of type integer between 1 and 100'),
-    check('cores').notEmpty().withMessage('cores can not be empty'),
+	check("modelName")
+		.notEmpty()
+		.isLength({ max: 100 })
+		.withMessage("modelName can not be empty"),
+	check("clockSpeed")
+		.isFloat({ min: 0.1, max: 100 })
+		.withMessage("clockspeed must be of type float, with a max size of 100"),
+	check("clockSpeed").notEmpty().withMessage("clockSpeed can not be empty"),
+	check("cores")
+		.isInt({ min: 1, max: 100 })
+		.withMessage("cores must be of type integer between 1 and 100"),
+	check("cores").notEmpty().withMessage("cores can not be empty"),
 ];
 
 module.exports = {
-    cpuModel,
-}
+	cpuModel,
+};
