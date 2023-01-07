f253592b28234105a02336eb9790e8099e74fc88
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-01-07 01:35:44 +0100

    basic cpudetailcard

--- app/src/components/Design/CpuDetailCard.js
+++ app/src/components/Design/CpuDetailCard.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const CpuDetailCard = ({ cpu }) => {
-  return <div>CpuDetailCard</div>;
+  return (
+    <table className="mt-4">
+      <tbody>
+        <tr>
+          <td>{cpu.manufacturerName}</td>
+        </tr>
+        <tr>
+          <td>{cpu.modelName}</td>
+        </tr>
+        <tr>
+          <td>{cpu.clockSpeed}</td>
+        </tr>
+        <tr>
+          <td>{cpu.cores}</td>
+        </tr>
+        <tr>
+          <td>{cpu.wattage ? cpu.wattage : "unknown"} W</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default CpuDetailCard;
