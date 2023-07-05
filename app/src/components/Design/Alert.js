c54c37d9869e6b40aa3ea5e7537c1a06a3f53598
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-07-05 19:04:37 +0200

    Added mapping over multiple alerts

--- app/src/components/Design/Alert.js
+++ app/src/components/Design/Alert.js
@@ -1,6 +1,22 @@
 import PropTypes from "prop-types";
 
 const Alert = ({ children, color = "primary" }) => {
+  if(Array.isArray(children) && children.length === 0) {
+    return
+  }
+  if(Array.isArray(children)) {
+    return (
+      <div className={`alert alert-${color}`} role="alert">
+        <ul>
+          {children.map((x, index) => {
+            return (
+              <li key={index}>{x.toString() || "Something went wrong"}</li>
+            );
+          })}
+        </ul>
+      </div>
+    )
+  }
   return (
     <div className={`alert alert-${color}`} role="alert">
       {children}
