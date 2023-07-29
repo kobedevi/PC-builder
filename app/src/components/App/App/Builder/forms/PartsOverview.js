1a5bc5f95a8b1f9590cade0865e59e40c914c067
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-07-29 02:00:26 +0200

    added memory model

--- app/src/components/App/App/Builder/forms/PartsOverview.js
+++ app/src/components/App/App/Builder/forms/PartsOverview.js
@@ -4,13 +4,15 @@ import CpuModel from "../../../Cpus/detail/Model/Model"
 import CpuCoolerModel  from "../../../CpuCoolers/detail/Model/Model"
 import MotherboardModel  from "../../../Motherboards/detail/Model/Model"
 import GpuModel from "../../../Gpu/detail/Model/Model"
+import MemoryModel from "../../../Ram/detail/Model/Model"
 
 const PartsOverview = ({currentBuild}) => {
 
   currentBuild = {
     ...currentBuild,
-    gpu: {
-      ...currentBuild.gpu,
+    ram: {
+      ...currentBuild.ram,
+      stickAmount: currentBuild.motherboard.memorySlots,
     }
   }
 
@@ -29,6 +31,7 @@ const PartsOverview = ({currentBuild}) => {
           <CpuCoolerModel cooler={currentBuild.cpucooler} scale={.8} position={[0.005,1.08,-.2]} rotation={[Math.PI / 2,0, 0]} />
           <MotherboardModel motherboard={currentBuild.motherboard} />
           <GpuModel gpu={currentBuild.gpu} position={[-2.5 + (currentBuild.gpu.width/100), -.15+ (-currentBuild.gpu.height/100), currentBuild.gpu.depth/100 - .1]} scale={1} rotation={[-Math.PI / 2,0, 0]}/>
+          <MemoryModel ram={currentBuild.ram} scale={1.23} ramOffset={.165} position={[1.68,1,.11]} rotation={[ 0, -Math.PI / 2, -Math.PI / 2]}/>
         </Layout>
       </div>
     </>
--- app/src/components/App/Ram/detail/Model/Model.js
+++ app/src/components/App/Ram/detail/Model/Model.js
@@ -1,19 +1,74 @@
-import { Text, useGLTF } from "@react-three/drei";
+import { Mask, Text, useGLTF } from "@react-three/drei";
 import React from "react";
-import cpuModel from "./assets/cpu.glb";
+import {Memory} from "./assets/Memory";
 
-const Model = ({ ram }) => {
-  const { scene: cpuScene } = useGLTF(cpuModel, true);
-
+const Model = ({ ram, scale=2, rotation=[0,0,0], position=[0,0,0], ramOffset=.23 }) => {
+  console.log(ram)
+  
   return (
-    <group name="cpu">
-      <group name="base" position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <primitive object={cpuScene} />
-      </group>
-      <group name="info">
-        <mesh position={[0, 0.15, -0.09]} receiveShadow={true}>
-          <React.Suspense fallback={null}>{/* TODO: MODEL */}</React.Suspense>
-        </mesh>
+    <group name="manipulator" scale={scale} rotation={rotation} position={position}>
+      <group name="ramSticks">
+        {Array(ram.stickAmount)
+          .fill(null)
+          .map((value, index) => {
+            return (
+              <group name="ramStick" position={[0,0,index * ramOffset]}>
+                <React.Suspense fallback={null}>
+                  <Text
+                    position={[-.85, .3, 0]}
+                    // Google fonts now only uses woff2 which isnt supported, solution is to host own woff files but cors policy gets in the way for localhost
+                    font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+                    color={0x666666}
+                    fontSize={0.07}
+                    anchorX={"left"}
+                  >
+                    {ram.ramType}
+                  </Text>
+                  <Text
+                    position={[-.85, 0.25, 0]}
+                    // Google fonts now only uses woff2 which isnt supported, solution is to host own woff files but cors policy gets in the way for localhost
+                    font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+                    color={0xaaaaaa}
+                    fontSize={0.03}
+                    anchorX={"left"}
+                    anchorY={"top"}
+                  >
+                    {ram.sizePerStick}GB
+                  </Text>
+                  <Text
+                    maxWidth={.4}
+                    position={[-.25, 0.03, 0]}
+                    // Google fonts now only uses woff2 which isnt supported, solution is to host own woff files but cors policy gets in the way for localhost
+                    font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+                    color={0xaaaaaa}
+                    fontSize={0.08}
+                    anchorX={"right"}
+                    anchorY={"bottom"}
+                    textAlign={"right"}
+                  >
+                    {ram.modelName} 
+                  </Text>
+                  <Text
+                    maxWidth={.7}
+                    position={[.2, 0.075, 0]}
+                    // Google fonts now only uses woff2 which isnt supported, solution is to host own woff files but cors policy gets in the way for localhost
+                    font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+                    color={0x666666}
+                    fontSize={0.1}
+                    anchorX={"left"}
+                    anchorY={"middle"}
+                    textAlign={"left"}
+                  >
+                    {ram.manufacturerName.toUpperCase()} 
+                  </Text>
+                </React.Suspense>
+                <group name="ram" rotation={[0, -Math.PI/2 ,0 ]}>
+                  <Memory/>
+                </group>
+              </group>
+            )
+          }
+        )}
       </group>
     </group>
   );
