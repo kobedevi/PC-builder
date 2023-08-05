eb325160193f4a9f61d3cadd155fc99bb164ab99
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-08-05 18:26:51 +0200

    removed buildId from itemList

--- app/src/components/Design/ItemList.js
+++ app/src/components/Design/ItemList.js
@@ -7,30 +7,32 @@ const ItemList = ({ info, color = "primary" }) => {
   return (
     <div className="ItemList">
       {Object.entries(info).map(([key, value], i) => {
-        return (
-          <div className="item" key={key}>
-            <h3><strong>{ObjectKeysToText(key)}</strong></h3>
-              {
-                (Array.isArray(value) && value.length > 0) &&
-                value.map((val, index) => {
-                  return (
-                    <div className="previewBox" key={index}>
-                      {val.image && <img style={{width: "100%", height: "auto", maxHeight: "300px", objectFit:"cover"}} src={val.image} alt="Product"/>}
-                      <ItemCollapser item={val}/>
+        console.log(key);
+        if (!key.startsWith("id")){
+          return (
+            <div className="item" key={key}>
+              <h3><strong>{ObjectKeysToText(key)}</strong></h3>
+                {
+                  (Array.isArray(value) && value.length > 0) &&
+                  value.map((val, index) => {
+                    return (
+                      <div className="previewBox" key={index}>
+                        {val.image && <img style={{width: "100%", height: "auto", maxHeight: "300px", objectFit:"cover"}} src={val.image} alt="Product"/>}
+                        <ItemCollapser item={val}/>
+                      </div>
+                    )
+                  })
+                }
+                {
+                  (!Array.isArray(value) && Object.keys(value).length > 0) && (
+                    <div className="previewBox">
+                      {value.image && <img style={{width: "100%", height: "auto", maxHeight: "300px", objectFit:"cover"}} src={value.image} alt="Product"/>}
+                      <ItemCollapser item={value}/>
                     </div>
                   )
-                })
-              }
-              {
-                (!Array.isArray(value) && Object.keys(value).length > 0) && (
-                  <div className="previewBox">
-                    {value.image && <img style={{width: "100%", height: "auto", maxHeight: "300px", objectFit:"cover"}} src={value.image} alt="Product"/>}
-                    <ItemCollapser item={value}/>
-                  </div>
-                )
-              }
-          </div>
-        )
+                }
+            </div>
+          )}
       })}
     </div>
   );
