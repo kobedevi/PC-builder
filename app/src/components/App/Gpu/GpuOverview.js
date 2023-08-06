31b63737e3372883f256c2966ea3b83ee5b770c7
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-08-06 20:16:36 +0200

    added isAdmin checks on gpu crud

--- app/src/components/App/Gpu/GpuOverview.js
+++ app/src/components/App/Gpu/GpuOverview.js
@@ -5,11 +5,12 @@ import Alert from "../../Design/Alert";
 import OriginalGpuOverview from "./Overview/OriginalGpu";
 import PartnerGpuOverview from "./Overview/PartnerGpu";
 import SearchForm from "components/Design/SearchForm";
+import useAdmin from "core/hooks/useAdmin";
 
 const GpuOverview = () => {
   const [info, setInfo] = useState();
   const [query, setQuery] = useState('');
-
+  const isAdmin = useAdmin();
 
   const onSubmit = (query) => {
     setQuery(query.search)
@@ -20,15 +21,19 @@ const GpuOverview = () => {
       <h2>GPU Overview</h2>
       {info && <Alert color="info">{info}</Alert>}
 
-      <Link to={PossibleRoutes.Create} className="btn btn-primary">
-        Add GPU
-      </Link>
+      {isAdmin && (
+        <Link to={PossibleRoutes.Create} className="btn btn-primary">
+          Add GPU
+        </Link>
+      )}
 
       <div>
-        <SearchForm
-          onSubmit={onSubmit}
-          setQuery={setQuery}
-        />
+        {isAdmin && (
+          <SearchForm
+            onSubmit={onSubmit}
+            setQuery={setQuery}
+          />
+        )}
         <OriginalGpuOverview query={query} setQuery={setQuery}/>
         <PartnerGpuOverview query={query} setQuery={setQuery}/>
       </div>
